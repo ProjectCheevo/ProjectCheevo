950429687b3bcddcc707d6d9c121fb55a4b2b3d6
ProjectCheevo <admin@projectcheevo.com>
2025-01-08 13:53:59 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-const form = document.querySelector("form");
-form.addEventListener("submit", function (event) {
-  event.preventDefault();
-  const passkey = document.querySelector("#passkey").value;
-  console.log("Passkey entered: ", passkey);
-});
+

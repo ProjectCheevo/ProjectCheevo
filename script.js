66a5bb5e3332056d307871067da802c6efd6e72f
ProjectCheevo <admin@projectcheevo.com>
2025-01-09 11:20:43 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
+  // Load the navbar
   fetch("nav.html")
     .then((response) => {
       if (!response.ok) {
@@ -8,6 +9,21 @@ document.addEventListener("DOMContentLoaded", function () {
     })
     .then((data) => {
       document.getElementById("navbar").innerHTML = data;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+
+  // Load the footer
+  fetch("footer.html")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Failed to load footer: " + response.statusText);
+      }
+      return response.text();
+    })
+    .then((data) => {
+      document.getElementById("footer").innerHTML = data;
     })
     .catch((error) => {
       console.error(error);

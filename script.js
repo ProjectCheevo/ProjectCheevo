45dde796f996535932edd5d51d705edb999e681c
ProjectCheevo <admin@projectcheevo.com>
2025-01-09 11:16:32 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("nav.html")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Failed to load navbar: " + response.statusText);
+      }
+      return response.text();
+    })
+    .then((data) => {
+      document.getElementById("navbar").innerHTML = data;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
